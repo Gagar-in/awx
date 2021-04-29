95d7450126c1e4823c3842b25f1616456e48e037
Andrei Tutolmin <a.tutolmin@gagar-in.com>
2021-04-29 17:46:03 +0300

    Results parser

---
+++ parse_results.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+RESULTS_DIR=/tmp
+cd $RESULTS_DIR
+
+for brd in `ls -d ????-????-????`; do
+
+  PROC=`cat $brd/gb5_report.json | jq .metrics[7].value`
+  MEM_TTL=`cat $brd/gb5_report.json | jq .metrics[23].value`
+
+  echo -e "######################################\n########## $brd ############\n######################################\n$PROC\n$MEM_TTL"
+
+  # Show memory part numbers
+  cat $brd/dmidecode.txt | grep Part\ Number | grep -v Not\ Specified 
+  
+  # get disk info
+  echo "############# Disks ###################"
+  cat $brd/lsblk.txt | grep -v MODEL=\"\"
+
+  # geekbench results
+  echo "################ Geekbench 5 ###########"
+  cat $brd/gb5_report.json | jq ".score, .multicore_score"
+
+  # FIO tests
+  echo "################ FIO ###########"
+  cat $brd/fio.json | jq '.jobs[].read.iops, .jobs[].read.slat_ns.mean, .jobs[].write.iops, .jobs[].write.slat_ns.mean'
+
+  # sysbench
+  echo "############# Sysbench ###################"
+  cat $brd/sysbench.txt |egrep "max|\/sec"
+
+done
